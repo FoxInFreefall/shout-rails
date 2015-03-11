e72d1bda0b9b21cd8525dce116e6c704c05cdb6f
MechaFox <aeseligman@gmail.com>
2015-03-11 15:52:54 -0700

    Add projects

---
+++ app/controllers/api/projects_controller.rb
@@ -0,0 +1,30 @@
+class Api::ProjectsController < ApplicationController
+
+  # skip_before_filter :verify_authenticity_token
+  respond_to :json
+
+  def create
+
+    project = Project.new(project_params)
+
+    if project.save
+      render :json => "{ id: " + project.id.to_s + " }", :status=>201
+    else
+      warden.custom_failure!
+      render :json => project.errors, :status=>422
+    end
+  end
+
+  def destroy
+
+    project = Project.find(params[:id])
+    project.destroy
+
+    render :json => project.errors, :status=>201
+  end
+
+  def project_params
+    params.require(:project).permit(:name)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :users
+    resources :projects
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
